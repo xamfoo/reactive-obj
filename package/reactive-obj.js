636f88aba9820b23861fcb901d8317f215379fe1
xamfoo <xamfoo@all.repo>
2015-05-25 07:02:23 +0800

    Always invalidate if cached value does not exist

--- package/reactive-obj.js
+++ package/reactive-obj.js
@@ -340,7 +340,7 @@ _.extend(ReactiveObj.prototype, {
 
     for (var i=0, l=depKeys.length, d; i<l; i+=1) {
       d = deps[depKeys[i]];
-      if (val !== d.lastVal) d.comp.invalidate();
+      if (!('lastVal' in d) || val !== d.lastVal) d.comp.invalidate();
     };
   }
 });
