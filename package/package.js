83ea7c76c9a25582e5ce6b31571d6204dda3acd3
xamfoo <xamfoo@all.repo>
2015-05-25 11:16:04 +0800

    Bump version to 0.3.0

--- package/package.js
+++ package/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'xamfoo:reactive-obj',
-  version: '0.2.0',
+  version: '0.3.0',
   // Brief, one-line summary of the package.
   summary: 'Reactivity for nested objects',
   // URL to the Git repository containing the source code for this package.
