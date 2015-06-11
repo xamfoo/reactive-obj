59d545b3b2d0ec8ccc8f3ef513a6617bf1242be5
xamfoo <xamfoo@all.repo>
2015-06-11 15:34:16 +0800

    Add helpers applyMutator and NOTSET

---
+++ package/helpers.js
@@ -0,0 +1,18 @@
+NOTSET = {};
+
+Helpers = {};
+
+Helpers.applyMutator = function (mutator, options, keyPath/*, arguments*/) {
+  options = options || {};
+  var self = this;
+  var node = Tracker.nonreactive(function () {
+    return self.get(keyPath, NOTSET);
+  });
+
+  if (node === NOTSET) node = undefined;
+  if (options.check && !options.check(node, keyPath)) return;
+
+  var result = mutator.apply(node, Array.prototype.slice.call(arguments, 3));
+  self.forceInvalidate(keyPath, {noChildren: true});
+  return result;
+};
