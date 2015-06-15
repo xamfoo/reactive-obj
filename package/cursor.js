b9d1ff8712fe6272dba5e331e322ad188165c77f
xamfoo <xamfoo@all.repo>
2015-06-15 14:51:48 +0800

    Add Helpers.applyCursorMethod

--- package/cursor.js
+++ package/cursor.js
@@ -11,15 +11,6 @@ var methodNames = [
   'get', 'equals', 'set', 'setDefault', 'update', 'forceInvalidate'
 ].concat(_.keys(ArrayMethods));
 
-function applyCursorMethod (methodName /*, arguments*/) {
-  var result = this._reactiveObj[methodName].apply(
-    this._reactiveObj,
-    [this._path].concat(Array.prototype.slice.call(arguments, 1))
-  );
-  if (result === this._reactiveObj) return this;
-  else return result;
-}
-
 _.each(methodNames, function (v) {
   Cursor.prototype[v] = _.partial(applyCursorMethod, v);
 });
--- package/helpers.js
+++ package/helpers.js
@@ -19,3 +19,27 @@ Helpers.applyMutator = function (mutator, options, keyPath/*, arguments*/) {
   self.forceInvalidate(keyPath, {noChildren: true});
   return result;
 };
+
+Helpers.applyCursorMethod = function (methodName, options, keyPath/*, arguments*/) {
+  options = options || {};
+  var self = this;
+  var args, results;
+
+  // Add cursor path as prefix if key path is given
+  if (options.noKeyPath &&
+      options.noKeyPath.indexOf(arguments.length - 2) >= 0) {
+    args = Array.prototype.slice.call(arguments, 2);
+    keyPath = self._path;
+  }
+  else {
+    args = Array.prototype.slice.call(arguments, 3);
+    keyPath = self._path.concat(self._reactiveObj._matchKeyPath(keyPath));
+  }
+
+  result = self._reactiveObj[methodName].apply(
+    self._reactiveObj,
+    [keyPath].concat(args)
+  );
+  if (result === self._reactiveObj) return self;
+  else return result;
+};
