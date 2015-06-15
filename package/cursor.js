216828426a448546cd760ea06daf3702179f34f4
xamfoo <xamfoo@all.repo>
2015-06-15 14:55:20 +0800

    Cursor methods detect if argument contains keyPath

--- package/cursor.js
+++ package/cursor.js
@@ -7,17 +7,17 @@ Cursor = function (reactiveObj, path) {
   this._path = path;
 }
 
-var methodNames = [
-  'get', 'equals', 'set', 'setDefault', 'update', 'forceInvalidate'
-].concat(_.keys(ArrayMethods));
-
-_.each(methodNames, function (v) {
-  Cursor.prototype[v] = _.partial(applyCursorMethod, v);
-});
-
-Cursor.prototype.select = function (keyPath) {
-  return this._reactiveObj.select.call(
-    this._reactiveObj,
-    this._path.concat(this._reactiveObj._matchKeyPath(keyPath))
-  );
+var methods = {
+  get: {noKeyPath: [0]},
+  equals: {noKeyPath: [1]},
+  set: {noKeyPath: [1]},
+  setDefault: {noKeyPath: [1]},
+  update: undefined,
+  select: undefined,
+  forceInvalidate: {noKeyPath: [1]}
 };
+_.each(ArrayMethods, function (v, k) { methods[k] = undefined; });
+
+_.each(methods, function (v, k) {
+  Cursor.prototype[k] = _.partial(Helpers.applyCursorMethod, k, v);
+});
